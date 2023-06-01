b53bebb927e521f0f7b9e153514971d9341dc99a
Charlie Holtz <charlieholtz@gmail.com>
2023-06-01 10:56:48 -0400

    tests pass

--- test/replicate_test.exs
+++ test/replicate_test.exs
@@ -22,26 +22,26 @@ defmodule ReplicateTest do
              "27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478"
   end
 
-  # test "create and wait prediction" do
-  #   {:ok, %Prediction{} = prediction} =
-  #     Replicate.Predictions.create(
-  #       "stability-ai/stable-diffusion:27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478",
-  #       prompt: "a 19th century portrait of a wombat gentleman"
-  #     )
+  test "create and wait prediction" do
+    {:ok, %Prediction{} = prediction} =
+      Replicate.Predictions.create(
+        "stability-ai/stable-diffusion:27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478",
+        prompt: "a 19th century portrait of a wombat gentleman"
+      )
 
-  #   {:ok, %Prediction{output: output}} = Replicate.Predictions.wait(prediction)
+    {:ok, %Prediction{output: output}} = Replicate.Predictions.wait(prediction)
 
-  #   assert output == [
-  #            "https://replicate.com/api/models/stability-ai/stable-diffusion/files/50fcac81-865d-499e-81ac-49de0cb79264/out-0.png"
-  #          ]
-  # end
+    assert output == [
+             "https://replicate.com/api/models/stability-ai/stable-diffusion/files/50fcac81-865d-499e-81ac-49de0cb79264/out-0.png"
+           ]
+  end
 
-  # test "run prediction" do
-  #   assert Replicate.run(
-  #            "stability-ai/stable-diffusion:27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478",
-  #            prompt: "a 19th century portrait of a wombat gentleman"
-  #          ) == [
-  #            "https://replicate.com/api/models/stability-ai/stable-diffusion/files/50fcac81-865d-499e-81ac-49de0cb79264/out-0.png"
-  #          ]
-  # end
+  test "run prediction" do
+    assert Replicate.run(
+             "stability-ai/stable-diffusion:27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478",
+             prompt: "a 19th century portrait of a wombat gentleman"
+           ) == [
+             "https://replicate.com/api/models/stability-ai/stable-diffusion/files/50fcac81-865d-499e-81ac-49de0cb79264/out-0.png"
+           ]
+  end
 end
